c79c86f4036b4aaa48ac31f5a7f14bf29810b416
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-19 14:29:40 -0600

    adding promps

--- Main/db/cli.js
+++ Main/db/cli.js
@@ -50,3 +50,110 @@ class CLI {
             })
 
     }}
+
+    viewDepartments() {
+        const sql = `SELECT * FROM department`;
+        db.query(sql, (err, rows) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.table(rows);
+            this.run();
+        }
+        )
+    }
+
+    viewRoles() {
+        const sql = `SELECT * FROM roles`;
+        db.query(sql, (err, rows) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.table(rows);
+            this.run();
+        }
+
+        )
+    }
+
+    viewEmployees() {
+        const sql = `SELECT * FROM employee`;
+        db.query(sql, (err, rows) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.table(rows);
+            this.run();
+        }
+
+        )
+    }
+
+    addDepartment() {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'department_name',
+                message: 'What is the name of the department?'
+
+            }
+        ])
+            .then(({ department_name }) => {
+                const sql = `INSERT INTO department (department_name)
+                VALUES (?)`;
+                const params = [department_name];
+
+                db.query(sql, params, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log('Department added!');
+                    this.run();
+                });
+            }
+
+            )
+    }
+
+    addRole() {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of the role?'
+
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role?'
+
+            },
+            {
+                type: 'input',
+                name: 'department_id',
+                message: 'What is the department id of the role?'
+
+            }
+        ])
+            .then(({ title, salary, department_id }) => {
+                const sql = `INSERT INTO roles (title, salary, department_id)
+                VALUES (?,?,?)`;
+                const params = [title, salary, department_id];
+
+                db.query(sql, params, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log('Role added!');
+                    this.run();
+                });
+            }
+
+            )
+    }
