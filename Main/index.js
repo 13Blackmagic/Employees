3c036c510d2d2f1bca77ad2265672ad534062d73
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-21 14:30:42 -0600

    adding

--- Main/index.js
+++ Main/index.js
@@ -283,11 +283,35 @@ function renderEmployees() {
 }
 
 function init() {
-    renderWelcome();
-    renderMenu();
-    renderDepartments();
-    renderRoles();
-    renderEmployees();
+    inquirer.prompt(userPrompt)
+    .then((response) => {
+        switch (response.action) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                exit();
+                break;
+        }
+    });
 }
 
 
